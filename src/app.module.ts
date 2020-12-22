159e8adb79663b5a408651c28728dd6878e84b86
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2020-12-22 20:47:59 -0300

    :wrench: create initial domain modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from 'src/app.controller';
 import { AppService } from 'src/app.service';
+import { ChallengeModule } from './challenge/challenge.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [ChallengeModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
